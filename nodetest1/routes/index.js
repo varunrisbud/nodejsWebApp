934b031511d8c4af69698dc9075f1d602aedfc5a
varunrisbud <varun.risbud@gmail.com>
2015-11-19 22:52:58 +0000

    added all routes and mongo functions

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express1' });
+	res.render('index', { title: 'Express1' });
+});
+
+router.get('/ipaddr', function(req, res, next) {
+	var interfaces = os.networkInterfaces();
+	var addresses = [];
+	for (var k in interfaces) {
+    		for (var k2 in interfaces[k]) {
+        		var address = interfaces[k][k2];
+        		if (address.family === 'IPv4' && !address.internal) {
+            			addresses.push(address.address);
+        		}
+    		}
+	}
+	res.json({ipaddress: addresses[0]});	
+});
+
+router.get('/list', function(req, res) {
+    var db = req.db;
+    var collection = db.get('furniture');
+    collection.find({},{},function(e,docs){
+        res.json({documents: docs});
+    });
+});
+
+router.get('/list1', function(req, res) {
+    var db = req.db;
+    var collection = db.get('furniture');
+    var nameToFind = req.param('name');
+    collection.find({name: nameToFind },{},function(e,docs){
+        res.json({documents: docs});
+    });
 });
 
 module.exports = router;
